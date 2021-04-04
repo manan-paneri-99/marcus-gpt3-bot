0b907f5eed5fc721e8ad876a676dc098eae2fb01
Manan Paneri <39223931+manan-paneri-99@users.noreply.github.com>
2021-04-04 22:00:19 +0530

    update: app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, request, session
 from twilio.twiml.messaging_response import MessagingResponse
 from chatbot import ask, append_interaction_to_chat_log
 
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'kEy@secReT_marcus'
+app.config['SECRET_KEY'] = 'your_key' # edit to enter new key
 
 
 @app.route('/bot', methods=['POST'])
